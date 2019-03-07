f26f97daa8d5f3fd4ef4e154d7267943a34b1311
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-03-07 14:35:02 -0600

    Refactor to  functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1 className="title">Hello World!</h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <h1 className="title">Hello World!</h1>
+    </div>
+  );
 }
 export default hot(module)(App);
