eddb6372a86007fe5dd9dc053a1cc117d9c2b542
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-03-07 16:28:01 -0600

    Without yarn.lock and update var to const

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import DefaultErrorBoundary from './components/DefaultErrorBoundary';
 import './styles.css';
 
 if (process.env.NODE_ENV !== 'production') {
-  var axe = require('react-axe');
+  const axe = require('react-axe');
   axe(React, ReactDOM, 1000);
 }
 
