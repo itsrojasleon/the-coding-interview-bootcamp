60527ed07718759e9c6481a33a574bcc290bbb05
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-04-09 15:07:58 -0500

    maxChar (5) added

--- src/components/exercises/04.tsx
+++ src/components/exercises/04.tsx
@@ -16,7 +16,36 @@ function MaxChar() {
         Given a string, return the character that is mostcommonly used in the
         string.
       </Steps>
-      <Code>S+</Code>
+      <Code>
+        <Keyword>function</Keyword>(<Parameter>str</Parameter>){'{'}
+        <MarginLeft>
+          <Keyword>const</Keyword> charMap = {'{}'}
+          <Jump jumps={1} />
+          <Keyword>let</Keyword> max = 0
+          <Jump jumps={1} />
+          <Keyword>let</Keyword> maxChar = <String />
+          <Jump jumps={2} />
+          <Keyword>for</Keyword> (<Keyword>let</Keyword> char of str){'{'}
+          <MarginLeft>charMap[char] = charMap[char] + 1 || 1</MarginLeft>
+          {'}'}
+          <Jump jumps={1} />
+          <Keyword>for</Keyword> (<Keyword>let</Keyword> char in charMap){'{'}
+          <MarginLeft>
+            <Keyword>if</Keyword> (charMap[char] > max) {'{'}
+            <MarginLeft>
+              max = charMap[char]
+              <Jump jumps={1} />
+              maxChar = char
+            </MarginLeft>
+            {'}'}
+          </MarginLeft>
+          {'}'}
+          <Jump jumps={1} />
+          <Keyword>return</Keyword> maxChar
+        </MarginLeft>
+        {'}'}
+      </Code>
+      <ExternalUrl url="https://repl.it/@rojasleon/MaxChar" />
     </div>
   );
 }
