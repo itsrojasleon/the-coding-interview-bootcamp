5573e29b22324d7f173f583b76045b43596ae101
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-04-08 20:50:40 -0500

    Structure for maxChar (4)

--- src/components/exercises/04.tsx
+++ src/components/exercises/04.tsx
@@ -1,10 +1,27 @@
 import * as React from 'react';
+import Steps from '../markdown/Steps';
+import Code from '../markdown/Code';
+import Jump from '../markdown/Jump';
+import Keyword from '../markdown/Keyword';
+import Parameter from '../markdown/Parameter';
+import Method from '../markdown/Method';
+import String from '../markdown/String';
+import MarginLeft from '../markdown/MarginLeft';
+import ExternalUrl from '../markdown/ExternalUrl';
 
-function ReverseString4() {
-  return <div>ReverseString 4</div>;
+function MaxChar() {
+  return (
+    <div>
+      <Steps>
+        Given a string, return the character that is mostcommonly used in the
+        string.
+      </Steps>
+      <Code>S+</Code>
+    </div>
+  );
 }
 function Usage() {
-  return <ReverseString4 />;
+  return <MaxChar />;
 }
-Usage.title = 'Reverse String 4';
-export { ReverseString4, Usage as default };
+Usage.title = 'Max char';
+export { MaxChar, Usage as default };
