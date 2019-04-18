3f5badd0680ac20ee5189a54018abd4b8e185806
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-04-18 14:30:53 -0500

    new theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,16 +3,18 @@ import * as React from 'react';
 function Header() {
   return (
     <div className="header">
-      <h1 className="title-bootcamp">The coding interview bootcamp</h1>
-      <div className="links">
-        <a
-          href="https://github.com/rojasleon/the-coding-interview-bootcamp"
-          target="_blank"
-          className="link-to-github"
-        >
-          <i className="fab fa-github" />
-        </a>
-      </div>
+      <nav className="nav">
+        <h1 className="title-bootcamp">The coding interview bootcamp</h1>
+        <div className="links">
+          <a
+            href="https://github.com/rojasleon/the-coding-interview-bootcamp"
+            target="_blank"
+            className="link-to-github"
+          >
+            <i className="fab fa-github" />
+          </a>
+        </div>
+      </nav>
     </div>
   );
 }
