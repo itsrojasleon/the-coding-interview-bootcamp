197c98992f92438608039969658347e2ca84ec8a
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-05-04 20:30:00 -0500

    Starter for 07-Anagrams

---
+++ src/components/exercises/07.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import AnagramsMDX from '../content/07-anagrams.mdx';
+import ExternalUrl from '../ExternalUrl';
+
+function Anagrams() {
+  return (
+    <div>
+      <AnagramsMDX />
+      <ExternalUrl url="" />
+    </div>
+  );
+}
+export default Anagrams;
