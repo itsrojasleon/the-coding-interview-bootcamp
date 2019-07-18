618595e15e25b7d9312ae980e6b182752d1a0a72
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-18 17:58:28 -0500

    Starting with new template

--- app/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Layout } from './layout';
-export { default as Header } from './header';
-export { default as SEO } from './seo';
-export { default as Image } from './image';
-export { default as SideBar } from './side-bar';
-export { default as Content } from './content';
